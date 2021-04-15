3ba2e1177b1184478d0b7a8f3e1fcd50e9c290a0
calebzulawski <calebzulawski@users.noreply.github.com>
2021-04-15 18:47:36 +0000

    deploy: da0bca76d1ac73e5db87e094eef62eda175326b8

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pep440_parser"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"pep440_parser/scheme/struct.Error.html\" title=\"struct pep440_parser::scheme::Error\">Error</a>","synthetic":false,"types":["pep440_parser::scheme::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
